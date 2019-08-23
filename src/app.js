096809930406744f4f8d6d7332d3b7061db0d377
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-08-23 07:55:37 -0500

    fixed small error

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
 	let response
-	if if (NODE_ENV === 'production') {
+	if (NODE_ENV === 'production') {
 		response = { error: { message: 'server error' } }
 	} else {
 		console.error(error)
