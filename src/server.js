24a49db85678124149870d91358902dd9590a130
Rachael Shaw <rachaelleeshaw@gmail.com>
2020-01-26 09:37:25 -0600

    updated with router and with services

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-const app = require('./app')
+// const app = require('./app')
+// const knex = require('knex')
+// const DB_URL = process.env.DB_URL
 
-const { PORT } = require('./config')
+// const { PORT, DATABASE_URL } = require('./config')
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+// const db = knex({ client: 'pg', connection: DATABASE_URL, })
+
+// app.set('db', db)
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
